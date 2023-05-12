331239be24dd8d0127a347a981996fae732ce159
SahilGyanchandani <gyanchandanisahil8@gmail.com>
2023-05-12 16:36:14 +0530

    Day 5 ex-1

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            using (var lggr = new FileLogger("log.txt"))
+            {
+                lggr.Log("entry 1");
+                lggr.Log("entry 2");
+            }
         }
     }
 
@@ -15,16 +20,34 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer= new StreamWriter(filePath,true);
         }
-
+        private bool disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing) {  
+                    _writer.Dispose();
+                }
+                disposed = true;
+            }
+        }
         public void Dispose()
         {
             // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         public void Log(string message)
         {
             // Write message to the file
+            if (disposed)
+                throw new ObjectDisposedException(nameof(FileLogger));
+            _writer.WriteLine(message);
+                 
+            
         }
     }
 }
