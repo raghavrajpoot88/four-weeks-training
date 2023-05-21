ae46e2ea8d178bfa707c068dd0bb736c5f920da8
raghavrajpoot88 <raghavrajpoot0115@gmail.comm>
2023-05-21 13:09:37 +0530

    Day 2 Solution

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             // Create a Circle object and display its area and circumference
+          
+            double radius = 5.0; // Replace with the desired radius
+
+            Circle circle = new Circle(radius);
+
+            double area = circle.GetArea();
+            double circumference = circle.GetCircumference();
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Circumference: " + circumference);
+            
         }
     }
 }
