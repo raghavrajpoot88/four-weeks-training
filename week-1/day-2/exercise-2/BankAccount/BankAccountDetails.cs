d07a3ba7783b4b8619339c13b77db8d9e1421053
raghavrajpoot88 <raghavrajpoot0115@gmail.comm>
2023-05-21 14:38:16 +0530

    Day 2 Exercise 2 Solution

---
+++ week-1/day-2/exercise-2/BankAccount/BankAccountDetails.cs
@@ -0,0 +1,67 @@
+﻿using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public abstract class BankAccountDetails
+    {
+        public string? AccountNumber { get; set; }
+        public decimal Balance { get; protected set; }
+
+        public abstract void Deposit(decimal amount);
+        public abstract void Withdraw(decimal amount);
+    }
+}
+
+class SavingsAccount : BankAccountDetails
+{
+    public decimal InterestRate { get; set; }
+
+    public override void Deposit(decimal amount)
+    {
+        Balance += amount;
+        Console.WriteLine($"Deposited Amount: {amount:C}. New balance: {Balance:C}");
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        if (amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn Amount: {amount:C}. New balance: {Balance:C}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient Balance.");
+        }
+    }
+}
+
+class CheckingAccount : BankAccountDetails
+{
+    public decimal OverdraftLimit { get; set; }
+
+    public override void Deposit(decimal amount)
+    {
+        Balance += amount;
+        Console.WriteLine($"Deposited Amount: {amount:C}. New balance: {Balance:C}");
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        if (amount <= Balance + OverdraftLimit)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn Amount: {amount:C}. New balance: {Balance:C}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance.");
+        }
+    }
+    
+}
--- week-1/day-2/exercise-2/BankAccount/Program.cs
+++ week-1/day-2/exercise-2/BankAccount/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             // Create SavingsAccount and CheckingAccount objects and perform operations
-        }
+            SavingsAccount savingsAccount = new SavingsAccount
+            {
+                AccountNumber = "Saving0001",
+                InterestRate = 0.05m
+            };
+
+            CheckingAccount checkingAccount = new CheckingAccount
+            {
+                AccountNumber = "CUrrent001",
+                OverdraftLimit = 500.00m
+            };
+
+            savingsAccount.Deposit(10000.00m);
+            savingsAccount.Withdraw(2000.00m);
+
+            checkingAccount.Deposit(5000.00m);
+            checkingAccount.Withdraw(6000.00m);
+            Console.ReadLine();
+        }   
     }
 }
