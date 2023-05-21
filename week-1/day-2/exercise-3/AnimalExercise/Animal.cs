902ca6eb5d9cf15956d84a8ff980584d17e527ed
raghavrajpoot88 <raghavrajpoot0115@gmail.comm>
2023-05-21 14:51:30 +0530

    Day 2 Exercise 3 Solution

---
+++ week-1/day-2/exercise-3/AnimalExercise/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public abstract class Animal
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public abstract void MakeSound();
+    }
+    class Dog : Animal, IMovable
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Wooof");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Running");
+        }
+    }
+    class Cat : Animal, IMovable
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meoow");
+        }
+        public void Move()
+        {
+            Console.WriteLine("Walking");
+        }
+    }
+    interface IMovable
+    {
+        void Move();
+    }
+
--- week-1/day-2/exercise-3/AnimalExercise/Program.cs
+++ week-1/day-2/exercise-3/AnimalExercise/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             // Create a list of Animal objects, add Dog and Cat instances, and call their methods
+            List<Animal> animals = new List<Animal>();
+
+            Dog dog = new Dog
+            {
+                Name = "Coco",
+                Age = 1
+            };
+
+            Cat cat = new Cat
+            {
+                Name = "JoJo",
+                Age = 3
+            };
+
+            animals.Add(dog);
+            animals.Add(cat);
+
+            foreach (Animal animal in animals)
+            {
+                animal.MakeSound();
+
+                if (animal is IMovable movable)
+                {
+                    movable.Move();
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
